8859f99cdf96d16381d69caf67e8f6ab62e3d2a5
Outrun <401047902@qq.com>
2022-06-07 19:30:56 +0800

    生成树优化

--- src/main/java/algorithm/exp_5/Bridge.java
+++ src/main/java/algorithm/exp_5/Bridge.java
@@ -1,6 +1,5 @@
 package algorithm.exp_5;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -15,15 +14,18 @@ public class Bridge {
     static int vertex; // 点的数目
     static List<int[]> res;
     static boolean[] visit;
-    static Map<Integer, Integer>[] position;
-    static int[] father;
+    static Map<Integer, Integer>[] position; // 一个点在另一个点的哪个位置（该点上的第几条边连接另一点）
+    static int[] father; // 并查集
+    static List<List<Integer>> tree; // 生成树
     static void create(int v) {
         vertex = v;
         adj = new ArrayList<>();
+        tree = new ArrayList<>();
         position = new HashMap[vertex];
         father = new int[vertex];
         for (int i = 0; i < vertex; i++) {
             adj.add(new ArrayList<>());
+            tree.add(new ArrayList<>());
             position[i] = new HashMap<>();
         }
         res = new ArrayList<>();
@@ -70,7 +72,46 @@ public class Bridge {
         }
     }
 
-    static int getParent(int i) {
+    static void createTree() {
+        for (int i = 0; i < adj.size(); i++) {
+            if (visit[i])
+                continue;
+            treeDFS(i);
+        }
+    }
+
+    private static void treeDFS(int i) {
+        visit[i] = true;
+        List<Integer> list = adj.get(i);
+        for (int index = 0; index < list.size(); index++) {
+            int j = list.get(index);
+            if (!visit[j]) {
+                tree.get(i).add(index);
+                treeDFS(j);
+            }
+        }
+    }
+
+    static void treeBFS() {
+        for (int i = 0; i < tree.size(); i++) {
+            List<Integer> list = tree.get(i);
+            List<Integer> listI= adj.get(i);
+            for (int index : list) {
+                // 通过使用生成树+并查集，减少判断边的次数
+                int j = listI.remove(index);
+                List<Integer> listJ = adj.get(j);
+                listJ.remove((int) position[j].get(i));
+                createSet();
+                listI.add(index, j);
+                listJ.add(position[j].get(i), i);
+                if (getParent(i) != getParent(j))
+                    res.add(new int[] {i, j});
+                Arrays.fill(visit, false);
+            }
+        }
+    }
+
+    private static int getParent(int i) {
         int parent = i;
         while (father[parent] != parent) {
             parent = father[parent];
@@ -90,17 +131,17 @@ public class Bridge {
                 if (listI.get(index) < i)
                     continue;
                 // 通过比较连通分量判断是否为桥
-                //int before = calculateConnect();
-                //Arrays.fill(visit, false);
-                //int j = listI.remove(index);
-                //List<Integer> listJ = adj.get(j);
-                //listJ.remove((int) position[j].get(i));
-                //int after = calculateConnect();
-                //Arrays.fill(visit, false);
-                //listI.add(index, j);
-                //listJ.add(position[j].get(i), i);
-                //if (before != after)
-                //    res.add(new int[] {i, j});
+                int before = calculateConnect();
+                Arrays.fill(visit, false);
+                int j = listI.remove(index);
+                List<Integer> listJ = adj.get(j);
+                listJ.remove((int) position[j].get(i));
+                int after = calculateConnect();
+                Arrays.fill(visit, false);
+                listI.add(index, j);
+                listJ.add(position[j].get(i), i);
+                if (before != after)
+                    res.add(new int[] {i, j});
 
                 // 通过删除边后能否搜索到另一点判断是否为桥
                 //int j = listI.remove(index);
@@ -114,37 +155,32 @@ public class Bridge {
                 //Arrays.fill(visit, false);
 
                 // 通过使用并查集，判断删除边后是否处于同一集合
-                int j = listI.remove(index);
-                List<Integer> listJ = adj.get(j);
-                listJ.remove((int) position[j].get(i));
-                createSet();
-                listI.add(index, j);
-                listJ.add(position[j].get(i), i);
-                if (getParent(i) != getParent(j))
-                    res.add(new int[] {i, j});
-                Arrays.fill(visit, false);
+                //int j = listI.remove(index);
+                //List<Integer> listJ = adj.get(j);
+                //listJ.remove((int) position[j].get(i));
+                //createSet();
+                //listI.add(index, j);
+                //listJ.add(position[j].get(i), i);
+                //if (getParent(i) != getParent(j))
+                //    res.add(new int[] {i, j});
+                //Arrays.fill(visit, false);
             }
         }
     }
 
-    static int calculateConnect() {
+    private static int calculateConnect() {
         int sum = 0;
         for (int i = 0; i < adj.size(); i++) {
             if (visit[i])
                 continue;
-            visit[i] = true;
             sum++;
-            List<Integer> list = adj.get(i);
-            for (int j : list) {
-                if (!visit[j]) {
-                    DFS(j);
-                }
-            }
+            DFS(i);
         }
         return sum;
     }
 
-    static void DFS(int i) {
+    private static void DFS(int i) {
+
         visit[i] = true;
         List<Integer> list = adj.get(i);
         for (int j : list) {
@@ -156,45 +192,59 @@ public class Bridge {
 
     public static void main(String[] args) throws IOException {
 
-        File file1 = new File("C:\\Users\\hp\\Desktop\\课件\\大三下\\算法\\实验5\\largeG.txt");
-        File file2 = new File("C:\\Users\\hp\\Desktop\\课件\\大三下\\算法\\实验5\\mediumG.txt");
-        File test = new File("C:\\Users\\hp\\Desktop\\课件\\大三下\\算法\\实验5\\test.txt");
-        Scanner reader;
-        Scanner scanner = new Scanner(System.in);
-        int file = scanner.nextInt();
+        //File file1 = new File("C:\\Users\\hp\\Desktop\\课件\\大三下\\算法\\实验5\\largeG.txt");
+        //File file2 = new File("C:\\Users\\hp\\Desktop\\课件\\大三下\\算法\\实验5\\mediumG.txt");
+        //File test = new File("C:\\Users\\hp\\Desktop\\课件\\大三下\\算法\\实验5\\test.txt");
+        //Scanner reader;
+        //Scanner scanner = new Scanner(System.in);
+        //int file = scanner.nextInt();
         int t = 20;
         long time = 0;
-        while (t-- > 0) {
+        for (int k = 0; k < t; k++) {
 
             long startTime, endTime;
 
             // 读取文件初始化邻接表
-            if (file == 1) {
-                reader = new Scanner(file1);
-            } else if (file == 2) {
-                reader = new Scanner(file2);
-            } else {
-                reader = new Scanner(test);
-            }
-
-            int vertex = reader.nextInt();
-            int line = reader.nextInt();
-            Bridge.create(vertex);
-            while(reader.hasNext()){
-                int i = reader.nextInt();
-                int j = reader.nextInt();
-                List<Integer> listi = Bridge.adj.get(i);
-                List<Integer> listj = Bridge.adj.get(j);
-                position[i].put(j, listi.size());
-                listi.add(j);
-                position[j].put(i, listj.size());
-                listj.add(i);
-            }
+            //if (file == 1) {
+            //    reader = new Scanner(file1);
+            //} else if (file == 2) {
+            //    reader = new Scanner(file2);
+            //} else {
+            //    reader = new Scanner(test);
+            //}
+            //
+            //int vertex = reader.nextInt();
+            //int line = reader.nextInt();
+            //Bridge.create(vertex);
+            //while(reader.hasNext()){
+            //    int i = reader.nextInt();
+            //    int j = reader.nextInt();
+            //    List<Integer> listi = Bridge.adj.get(i);
+            //    List<Integer> listj = Bridge.adj.get(j);
+            //    position[i].put(j, listi.size());
+            //    listi.add(j);
+            //    position[j].put(i, listj.size());
+            //    listj.add(i);
+            //}
 
             // 随机生成邻接表
-            //int vertex = 180;
-            //Bridge.create(vertex);
-            //Bridge.randomCreate(false);
+            int vertex = 180;
+            Bridge.create(vertex);
+            Bridge.randomCreate(true);
+
+            // 生成生成树
+            Bridge.createTree();
+
+            // 输出生成树
+            //System.out.println("生成树如下：");
+            //for (int i = 0; i < Bridge.tree.size(); i++) {
+            //    List<Integer> temp = Bridge.tree.get(i);
+            //    System.out.printf(i + ":");
+            //    for (int temp2 : temp) {
+            //        System.out.printf(temp2 + " ");
+            //    }
+            //    System.out.println();
+            //}
 
             // 输出邻接表
             //System.out.println("邻接表如下：");
@@ -207,18 +257,18 @@ public class Bridge {
             //    System.out.println();
             //}
             startTime = System.nanoTime();
-            Bridge.BFS();
+            Bridge.treeBFS();
             endTime = System.nanoTime();
             time += endTime - startTime;
             System.out.println("运行时间： " + (endTime - startTime) + "ns");
 
             // 输出桥
-            System.out.println("总共有" + Bridge.res.size() + "座桥，分别如下：");
-            for (int[] temp : Bridge.res) {
-                System.out.println(temp[0] + " " + temp[1]);
-            }
+            //System.out.println("总共有" + Bridge.res.size() + "座桥，分别如下：");
+            //for (int[] temp : Bridge.res) {
+            //    System.out.println(temp[0] + " " + temp[1]);
+            //}
         }
 
-        System.out.println("20次运行平均时间： " + time / 20 + "ns");
+        System.out.println(t + "次运行平均时间： " + time / t + "ns");
     }
 }
